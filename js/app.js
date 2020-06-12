41a023f400625940b4aaf47f270ba511a60fa4cc
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-13 00:42:53 +0900

    :sparkles: Log表示機能を実装

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ new Vue({
   el: "#app",
   data: {
     gameStarted: false,
-    existsLog: false,
+    logs: [],
     you: {
       hitPoint: 100,
       attack: 8,
@@ -14,14 +14,20 @@ new Vue({
     }
   },
   computed: {
+    reverseLogs: function() {
+      return this.logs.reverse()
+    },
+    displayLogs: function() {
+      return this.logs.length
+    }
   },
   methods: {
-    hitPointsStyle: function(character) {
+    hitPointsStyle: function(chara) {
       return {
         backgroundColor: 'green',
         margin: 0,
         color: 'white',
-        width: (character.hitPoint <= 0) ? '0px' : character.hitPoint + '%'
+        width: (chara.hitPoint <= 0) ? '0px' : chara.hitPoint + '%'
       }
     },
     randomValue: function() {
@@ -33,6 +39,7 @@ new Vue({
       this.gameStarted = !this.gameStarted
       this.you.hitPoint = 100
       this.monster.hitPoint = 100
+      this.logs = []
     },
     isEndGame: function() {
       if (this.you.hitPoint <= 0 ) {
@@ -46,19 +53,42 @@ new Vue({
         }
       }
     },
+    buildLog: function(fromChara, toChara, actionType, value) {
+      return `${fromChara} ${actionType} ${toChara} FOR ${value}`
+    },
     playAttack: function() {
-      this.monster.hitPoint -= Math.round(this.you.attack * this.randomValue())
-      this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+      attackToMonster = Math.round(this.you.attack * this.randomValue())
+      attachToYou     = Math.round(this.monster.attack * this.randomValue())
+
+      this.monster.hitPoint -= attackToMonster
+      this.you.hitPoint     -= attachToYou
+
+      this.logs.push(this.buildLog('PLAYER', 'MONSTER', 'HITS', attackToMonster))
+      this.logs.push(this.buildLog('MONSTER', 'PLAYER', 'HITS', attachToYou))
+      this.existsLog = true
       this.isEndGame()
     },
     playSpecialAttack: function() {
-      this.monster.hitPoint -= Math.round( (this.you.attack * 1.5) * this.randomValue())
-      this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+      attackToMonster = Math.round((this.you.attack * 1.5) * this.randomValue())
+      attachToYou     = Math.round(this.monster.attack * this.randomValue())
+
+      this.monster.hitPoint -= attackToMonster
+      this.you.hitPoint     -= attachToYou
+
+      this.logs.push(this.buildLog('PLAYER', 'MONSTER', 'HITS', attackToMonster))
+      this.logs.push(this.buildLog('MONSTER', 'PLAYER', 'HITS', attachToYou))
+      this.existsLog = true
       this.isEndGame()
     },
     playHeal: function() {
+      attachToYou = Math.round(this.monster.attack * this.randomValue())
+
       this.you.hitPoint += this.you.heal
       this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+
+      this.logs.push(this.buildLog('PLAYER', 'HIMSELF', 'HEALS', this.you.heal))
+      this.logs.push(this.buildLog('MONSTER', 'PLAYER', 'HITS', attachToYou))
+      this.existsLog = true
       this.isEndGame()
     }
   }
