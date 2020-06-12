db28543372e9a73933e7b24fb10728e53749506c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-13 00:14:40 +0900

    :sparkles: ゲームの終了処理を実装

--- js/app.js
+++ js/app.js
@@ -24,27 +24,42 @@ new Vue({
         width: (character.hitPoint <= 0) ? '0px' : character.hitPoint + '%'
       }
     },
-    startNewGame: function() {
-      this.gameStarted = !this.gameStarted
-      this.you.hitPoint = 100
-      this.monster.hitPoint = 100
-    },
     randomValue: function() {
       // ref: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_number_between_two_values
       const randomRange = 1.2
       return Math.random() * (randomRange - 1) + 1
     },
+    startNewGame: function() {
+      this.gameStarted = !this.gameStarted
+      this.you.hitPoint = 100
+      this.monster.hitPoint = 100
+    },
+    isEndGame: function() {
+      if (this.you.hitPoint <= 0 ) {
+        if (confirm('You lose! New Game?')) {
+          this.startNewGame()
+        }
+      }
+      if (this.monster.hitPoint <= 0 ) {
+        if (confirm('You win! New Game?')) {
+          this.startNewGame()
+        }
+      }
+    },
     playAttack: function() {
       this.monster.hitPoint -= Math.round(this.you.attack * this.randomValue())
       this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+      this.isEndGame()
     },
     playSpecialAttack: function() {
       this.monster.hitPoint -= Math.round( (this.you.attack * 1.5) * this.randomValue())
       this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+      this.isEndGame()
     },
     playHeal: function() {
       this.you.hitPoint += this.you.heal
       this.you.hitPoint -= Math.round(this.monster.attack * this.randomValue())
+      this.isEndGame()
     }
   }
 })
